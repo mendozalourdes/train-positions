b3f01253fd0eb355d5a2b74d1a31c856a68c1e12
mendozalourdes <lourd.mendoza@gmail.com>
2022-06-23 23:36:44 -0600

    Set up fetch call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import {fetchTrainPositions} from './apiCalls'
+import loadingSpin from "./Images/loadingSpin.gif";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+  const [trainPositions, setTrainPositions] = useState(null)
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(true)
+
+
+  const getTrainData = async () => {
+
+    setTrainPositions('')
+    try {
+        let trainData = await fetchTrainPositions()
+  
+        setTrainPositions(trainData)
+        setLoading(false)
+        console.log("trainData", trainData)
+      } catch (error) {
+        setError(error.message)
+    }
+  }
+
+  useEffect(() => {
+    getTrainData();
+  }, []);
+
+ 
+  if(trainPositions) {
+    return (
+
+      <main>
+        <div className="App">
+          <h1 className="title ">Train Positions</h1>
+        </div>
+      </main>
+    );
+
+  }
+
+  if(loading) {
+    return (
+      <main>
+        <div className="App">
+          <h1 className="title ">Train Positions</h1>
+          <section className="loading-container ">
+            <p className="loading-message title">Loading Train Positions</p>
+            <img className="loading" src={loadingSpin} />
+          </section>
+        </div>
+      </main>
+    );
+  }
+  
+};
 
 export default App;
