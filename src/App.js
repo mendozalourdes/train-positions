ccd557056c574e24df805035f6b414f9255674eb
mendozalourdes <lourd.mendoza@gmail.com>
2022-06-25 17:19:25 -0600

    Created filter functions, included new components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import {fetchTrainPositions} from './apiCalls'
+import {fetchTrainPositions} from './Utils/apiCalls'
 import loadingSpin from "./Images/loadingSpin.gif";
+import AllTrains from '../src/Components/AllTrains'
+import Form from './Components/NavBar'
 
 
 const App = () => {
   const [trainPositions, setTrainPositions] = useState(null)
   const [error, setError] = useState("")
   const [loading, setLoading] = useState(true)
-
+  const [time, setTime] = useState(Date.now());
+  const [filteredTrainPositions, setFilteredTrainPositions] = useState([])
+  
 
   const getTrainData = async () => {
 
@@ -18,23 +22,49 @@ const App = () => {
   
         setTrainPositions(trainData)
         setLoading(false)
-        console.log("trainData", trainData)
+        // console.log("trainData", trainData)
       } catch (error) {
         setError(error.message)
     }
   }
 
-  useEffect(() => {
-    getTrainData();
-  }, []);
+
+const handleFilter = (event, formType) => {
+
+  let allTrains = trainPositions
+  let filteredTrains = allTrains.filter((train) => {
+if(event == "null") {
+  return train[formType] == null
+
+} else {
+   return train[formType] == event
+}
+  })
+
+  setFilteredTrainPositions(filteredTrains)
+  return filteredTrainPositions
+}
+
+
+useEffect(() => {
+  const interval = setInterval(() => setTime(Date.now()), 7000);
+  getTrainData();
+  return () => {
+    clearInterval(interval);
+  };
+}, []);
+
+
 
  
-  if(trainPositions) {
+  if(trainPositions)  {
     return (
 
       <main>
         <div className="App">
           <h1 className="title ">Train Positions</h1>
+          <Form handleFilter={handleFilter} trainPositions={trainPositions}  filteredTrainPositions={filteredTrainPositions}/>
+          <AllTrains  filteredTrainPositions={filteredTrainPositions} trainPositions={trainPositions}/>
         </div>
       </main>
     );
